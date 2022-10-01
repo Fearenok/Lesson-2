b07ae94ce975c0894f25320cded52b81131ab6e3
Anton <polyncev1991@mail.ru>
2022-10-01 11:51:48 +0300

    Task 1

---
+++ Lesson 2/Task 1.py
@@ -0,0 +1,11 @@
+# 1. Напишите программу, которая принимает на вход вещественное число 
+# и показывает сумму его цифр.
+
+float_num = input('input float number: ')
+print(type(float_num))
+    
+sum = 0
+for i in float_num:
+    if i != '.':
+        sum += int(i)
+print(sum)
