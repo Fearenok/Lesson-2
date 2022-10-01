6a20e7a8a039a5f582969aed4472079eb4157177
Anton <polyncev1991@mail.ru>
2022-10-01 12:39:09 +0300

    Task 3

---
+++ Lesson 2/Task 3.py
@@ -0,0 +1,14 @@
+# 3. Задайте список из n чисел, заполненный по формуле 
+# (1 + 1/n) ** n и выведите на экран их сумму.
+
+
+from msilib import sequence
+
+n = int(input('Введите число: ')) 
+
+def get_squerence(n):
+    return [round((1 + 1 / x) ** x, 5) for x in range (1, n + 1)]
+
+nums = get_squerence(n)
+print(nums)
+print(round(sum(nums), 5))
